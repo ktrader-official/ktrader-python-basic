c4787785384fe11275d5bc4741db5d1b93fdbf19
kztrader <kennyzhang@zenquant.cn>
2022-06-15 18:14:36 +0800

    update double_ma.py

--- src/double_ma.py
+++ src/double_ma.py
@@ -1,3 +1,4 @@
+# 常用函数库
 import json
 import pytz
 from datetime import datetime
@@ -9,18 +10,20 @@ import numpy as np
 class DoubleMA(python_strategy):
     # DoubleMA 是最简模板策略，即是经常见到的双均线策略。
     def __init__(self):
-        # 每一个新的策略instance需要继承 python_strategy object
+        # Class自带的初始化函数，每一个新的策略创建时会执行这个函数，
+        # 可以在这里设置一些全局变量
         python_strategy.__init__(self)
         kt_info('Strategy Created: {}'.format(self))
         # Initialization
-        self.context = ''
-        self.param = None
-        self.target_open = position_target() # 初始化调仓object
+        self.context = '' # 初始化策略名称
+        self.param = None # 初始化策略参数
+        self.target_open = position_target() # 初始化目标仓位
 
     def update_config(self, cfg_path):
-        # 加载global_config.json
+        # 加载 global_config.json 和 策略参数文件 double_ma.json  
         kt_info('Loading strategy global config: {}'.format(cfg_path))
         f = open(cfg_path)
+        # 加载策略参数文件 double_ma.json
         params = json.load(f, object_hook=lambda d: SimpleNamespace(**d)) # load json strategy config into object
         self.context = params.context
         self.param = params.params
@@ -28,35 +31,17 @@ class DoubleMA(python_strategy):
         return ''
 
     def init(self):
-        # init 在每次交易刚开始时会触发, 用于策略初始化
+        # init 在每个交易日之前（回测）或策略启动时（实盘）触发, 用于策略初始化
         kt_info('Strategy Init: {}'.format(self.context))
+        # 订阅合约 
+        self.api.subscribe_instrument(self.param.symbol)
         current_time = format_time(get_time_now(), '')
-        action_day = datetime.fromtimestamp(get_time_now() / 1e9, pytz.timezone('Asia/Shanghai')).date().strftime('%Y-%m-%d') # action day 自然日
-        kt_info('current day is: {}, trading_day is: {}, time is: {}'.format(action_day, get_trading_day(), current_time)) #显示时间信息
+        current_day = datetime.fromtimestamp(get_time_now() / 1e9, pytz.timezone('Asia/Shanghai')).date().strftime('%Y-%m-%d') # 获得当前日（自然日)
+        kt_info('current day is: {}, trading_day is: {}, time is: {}'.format(current_day, get_trading_day(), current_time)) #显示时间信息
 
-        # account summary 显示账户信息
-        summary = self.api.get_account_summary()
-        acc_fees = summary.total_commission
-        acc_profit = summary.position_profit
-        acc_netpnl = summary.net_pnl
-        acc_netpnl_high = summary.net_pnl_high
-        acc_netpnl_low = summary.net_pnl_low
-        kt_info('Account Summary: 用户ID: {}, 持仓盈亏:{}, 手续费: {}, 净利润:{}, 最高净利: {}, 最低净利:{}'.format(summary.investor_id, acc_profit, acc_fees, acc_netpnl, acc_netpnl_high, acc_netpnl_low))
-
-        # position summary 显示本策略所有仓位信息
-        summary = self.api.get_position_summary()
-        kt_info('Position Summary: 平仓盈亏:{}, 持仓盈亏:{}, 总保证金:{}, 总手续费:{}, '.format(summary.close_profit, summary.position_profit, summary.total_margin, summary.total_commission))
-
-        # contract summary 显示单个合约持仓信息
-        kt_info('Contract Summary:')
-        pos = self.api.get_instrument_position_detail(self.param.symbol)
-        total_long = pos.long_position.total # 多仓(总)
-        history_long = pos.long_position.history # 昨多仓
-        total_short = -pos.short_position.total # 空仓(总)
-        history_short = -pos.short_position.history # 昨空仓
-        init_net_pos = total_long + total_short # 净多仓
-        kt_info('合约:{}, 多仓:{}, 昨多:{}, 空仓:{}, 昨空:{}, 净多仓:{}'.format(self.param.symbol, total_long, history_long, total_short, history_short, init_net_pos))
-
+        # account summary 显示账户信息示例
+        acc = self.api.get_account_summary()
+        kt_info('Account Summary: 用户ID: {}, 持仓盈亏:{}, 手续费: {}, 净利润:{}, 最高净利: {}, 最低净利:{}'.format(acc.investor_id, acc.position_profit, acc.total_commission, acc.net_pnl, acc.net_pnl_high, acc.net_pnl_low))
         return
 
     def shutdown(self):
@@ -65,46 +50,49 @@ class DoubleMA(python_strategy):
         return
 
     def on_tick(self, t):
-        # 对每一个Tick事件，做出相应交易动作
+        # 对每一个Tick事件，做出相应交易动作, 策略编写的主要
         tick_time = format_time(int(t.timestamp), '')
-        # get current netpnl
-        summary = self.api.get_account_summary()
-        netpnl = summary.net_pnl
-        kt_info('合约:{},时间:{},净利润:{},最新:{},最高:{},最低:{},买一:{},买一量:{},卖一:{},卖一量:{},成交量:{},增仓:{}'.format(
-            t.instrument_id, tick_time, netpnl, t.last_price, t.highest_price, t.lowest_price, t.bid_price[0], t.bid_volume[0], t.ask_price[0], t.ask_volume[0], t.volume_delta, t.open_interest_delta))
-
-        if self.param.symbol != t.instrument_id: # 检查symbol
+        # 获取当前tick时的账户净盈利(netpnl)
+        acc = self.api.get_account_summary()
+        # 打印出新来的tick 
+        kt_info('合约:{},时间:{},净利润:{},手续费:{},最新:{},最高:{},最低:{},买一:{},买一量:{},卖一:{},卖一量:{},成交量:{},增仓:{}'.format(
+            t.instrument_id, tick_time, acc.net_pnl, round(acc.total_commission,2), t.last_price, t.highest_price, t.lowest_price, t.bid_price[0], t.bid_volume[0], t.ask_price[0], t.ask_volume[0], t.volume_delta, t.open_interest_delta))
+            
+        if self.param.symbol != t.instrument_id: # 检查合约名称是否是当前策略给定的
             return
-        cur_price = t.last_price
+        
         summary = self.api.get_instrument_summary(self.param.symbol)
+        # 获取合约仓位    
         cur_net_pos = summary.net_pos
 
         # get moving averages
-        bars = self.api.get_last_k_bars(self.param.symbol, 25, 5) # bars[0]: 离当前最近的bar
+        bars = self.api.get_last_k_bars(self.param.symbol, 25, 1) # 获取25个，1分钟K线，
         close_prices = [bar.close for bar in bars]
-        slow_ma = np.mean(close_prices)
-        fast_ma = np.mean(close_prices[-5:])
+        fast_ma = np.mean(close_prices[-10:]) # MA 10 (快均线)
+        slow_ma = np.mean(close_prices) # MA 25 (慢均线)
         kt_info('fast_ma: {}, slow_ma:{}'.format(fast_ma, slow_ma))
 
         # 开仓条件
         if cur_net_pos == 0:
             # 买入开仓条件
-            if cur_price > fast_ma and fast_ma > slow_ma:
-                # 设定position_target object: symbol, algo type, target_pos, desired price
+            if t.last_price > fast_ma and fast_ma > slow_ma:
+                # 设置调仓目标：默认用基本(basic)算法，目标仓位为1（多1手），理想价格为最新价(t.last_price)
                 self.target_open.instrument_id = self.param.symbol
                 self.target_open.algorithm = target_position_algorithm.basic
                 self.target_open.target_pos = 1
-                self.target_open.desired_price = cur_price
+                self.target_open.desired_price = t.last_price
+                
             # 卖出开仓条件
-            elif cur_price < fast_ma and  fast_ma < slow_ma:
+            elif t.last_price < fast_ma and  fast_ma < slow_ma:
+                # 设置调仓目标：默认用基本(basic)算法，目标仓位为-1(空1手），理想价格为最新价(t.last_price)
                 self.target_open.instrument_id = self.param.symbol
                 self.target_open.algorithm = target_position_algorithm.basic
                 self.target_open.target_pos = -1
-                self.target_open.desired_price = cur_price
-
-        if not self.target_open.instrument_id:
-            return
-        self.api.set_target_position(self.target_open, False) # set target position 进行调仓
+                self.target_open.desired_price = t.last_price
+        
+        # 需要self.target_open.instrument_id不为空时，启动调仓函数
+        if self.target_open.instrument_id:
+            self.api.set_target_position(self.target_open, False) # 启动调仓函数
 
     def on_order_update(self, update):
         # 对于每一个订单更新或成交事件进行处理
